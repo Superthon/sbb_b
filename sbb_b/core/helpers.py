833c10028e3b2ada55dca0a40a2495b6a0394d77
Superthon <108042738+Superthon@users.noreply.github.com>
2022-06-23 08:27:08 +0300

    Update helpers.py

--- sbb_b/core/helpers.py
+++ sbb_b/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("sbb_b")
+LOGGER = logging.getLogger("Superthon")
 
 
 def printUser(entity: types.User) -> None:
